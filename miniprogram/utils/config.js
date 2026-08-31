4c5fd034c47e6e07b3e7c3a040b7a0c00de17000
niewei <544172444@qq.com>
2026-08-31 15:50:28 +0800

    chore: use development miniapp endpoints

--- miniprogram/utils/config.js
+++ miniprogram/utils/config.js
@@ -2,7 +2,7 @@
 module.exports = {
   // 本地运行
   // API_BASE_URL: 'http://localhost:8080/api/v1',
-  // 后端 API 线上 URL
+  // 后端 API 开发环境 URL
   API_BASE_URL: 'https://api-dev.xianzaimai.com/api/v1',
 
   // 小程序 AppID
